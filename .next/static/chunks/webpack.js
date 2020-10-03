ecca86b843e0344b27a0aa8ff0a4a8dd78eb4f26
Elie Hymowitz <ehymowitz@gmail.com>
2020-10-03 16:59:18 -0400

    init backend

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "760d484ea6467a6771ab";
+/******/ 	var hotCurrentHash = "8ac6f8a25341f0b98b3c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
