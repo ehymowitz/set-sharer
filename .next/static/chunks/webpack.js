d33482801974135214ba0e5a1146df827b1ea2fc
Elie Hymowitz <ehymowitz@gmail.com>
2020-10-03 18:50:47 -0400

    cleaned code, next step is db

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cfb4744091592a7e0f68";
+/******/ 	var hotCurrentHash = "d17cf078c07a087ffdc5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
