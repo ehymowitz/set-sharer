789828b7343b538c311587cd51a946a28de4b35a
Elie Hymowitz <ehymowitz@gmail.com>
2021-01-05 17:57:43 -0500

    Added buymeacoffee

--- components/sidePanel.tsx
+++ components/sidePanel.tsx
@@ -17,6 +17,18 @@ const SidePanel = () => {
         <small>(Inputs are not case sensitive)</small>
         <p style={{marginTop: '20px'}}>WARNING: This tool is very much in development. When you put information on here it very well could be deleted if I mess somthing up. So back up your data.</p>
         <p>I'm making this tool as a musician and for musicians, so if you have any suggestions for features feel free to reach me at ehymowitz@gmail.com </p>
+        <a
+          href="https://www.buymeacoffee.com/ehymowitz"
+          className="coffee"
+          target="_blank"
+          style={{ position: "absolute", top: "30px", right: "30px"}}
+        >
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+            alt="Buy Me A Coffee"
+            style={{height: "100%", width: "150px"}}
+          />
+        </a>
       </div>
     )
   } else {
