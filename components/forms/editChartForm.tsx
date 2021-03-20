18b518be0c3f39c0afdaa912b889e8521a752612
Elie Hymowitz <ehymowitz@gmail.com>
2021-03-19 20:25:48 -0400

    updated form rules

--- components/forms/editChartForm.tsx
+++ components/forms/editChartForm.tsx
@@ -1,57 +1,63 @@
-import React, { useState, useContext, SyntheticEvent, ChangeEvent } from 'react'
-import { DisplayedSong } from '../../pages/index'
-import { LoggedIn } from '../../pages/_app'
-import { updateSongNotes } from '../../utils/crud'
+import React, {
+  useState,
+  useContext,
+  SyntheticEvent,
+  ChangeEvent,
+} from "react";
+import { DisplayedSong } from "../../pages/index";
+import { LoggedIn } from "../../pages/_app";
+import { updateSongNotes } from "../../utils/crud";
 
-const EditChartForm = ({setOpen}) => {
-  const { displayedSong, setDisplayedSong } = useContext(DisplayedSong)
-  const { loggedIn } = useContext(LoggedIn)
-  const [chart, changeChart] = useState(displayedSong.notes.chart)
+const EditChartForm = ({ setOpen }) => {
+  const { displayedSong, setDisplayedSong } = useContext(DisplayedSong);
+  const { loggedIn } = useContext(LoggedIn);
+  const [chart, changeChart] = useState(displayedSong.notes.chart);
 
   const handleSubmit = (e: SyntheticEvent) => {
-    e.preventDefault()
-    const newNotes = displayedSong.notes
-    newNotes.chart = chart
+    e.preventDefault();
+    const newNotes = displayedSong.notes;
+    newNotes.chart = chart;
     updateSongNotes({
-      title: displayedSong.title,
-      artist: displayedSong.artist,
+      ...displayedSong,
+      set: loggedIn,
       notes: newNotes,
-      set: loggedIn
-    })
+    });
 
-    setOpen(false)
+    setOpen(false);
 
     setDisplayedSong({
       title: displayedSong.title,
       artist: displayedSong.artist,
-      notes: newNotes
-    })
+      notes: newNotes,
+    });
+  };
 
-  }
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
-    e.preventDefault()
-    changeChart(e.target.value)
-  }
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    e.preventDefault();
+    changeChart(e.target.value);
+  };
 
   return (
-    <form className="chart-edit-form" onSubmit={(e)=> handleSubmit(e)}>
+    <form className="chart-edit-form" onSubmit={(e) => handleSubmit(e)}>
       <div className="inputs">
         <input
-          onChange={(e)=> handleChange(e)}
+          onChange={(e) => handleChange(e)}
           type="text"
           value={chart}
           placeholder="Any PDF URL"
         />
-        <input type="submit" id="modal-submit" className="text-button" value="Submit"/>
+        <input
+          type="submit"
+          id="modal-submit"
+          className="text-button"
+          value="Submit"
+        />
       </div>
-      <iframe
-        src={chart}
-        frameBorder="0"
-        className = "chart-preview"
-      />
+      <iframe src={chart} frameBorder="0" className="chart-preview" />
     </form>
-  )
-}
+  );
+};
 
-export default EditChartForm
+export default EditChartForm;
--- components/forms/editSongForm.tsx
+++ components/forms/editSongForm.tsx
@@ -1,63 +1,81 @@
-import React, { useState, useContext, SyntheticEvent, ChangeEvent } from 'react'
-import { DisplayedSong } from '../../pages/index'
-import { LoggedIn } from '../../pages/_app'
-import { updateSongNotes } from '../../utils/crud'
+import React, {
+  useState,
+  useContext,
+  SyntheticEvent,
+  ChangeEvent,
+} from "react";
+import { DisplayedSong } from "../../pages/index";
+import { LoggedIn } from "../../pages/_app";
+import { updateSongNotes } from "../../utils/crud";
 
-const EditSongForm = ({setOpen}) => {
-  const { displayedSong, setDisplayedSong } = useContext(DisplayedSong)
-  const { loggedIn } = useContext(LoggedIn)
+const EditSongForm = ({ setOpen }) => {
+  const { displayedSong, setDisplayedSong } = useContext(DisplayedSong);
+  const { loggedIn } = useContext(LoggedIn);
   const [form, changeForm] = useState({
     lyrics: displayedSong.notes.lyrics,
     youtube: `https://www.youtube.com/watch?v=${displayedSong.notes.youtubeID}`,
     spotify: `https://open.spotify.com/track${displayedSong.notes.spotifyID}`,
     artwork: displayedSong.notes.spotifyAlbumCover,
-    soundcloud: displayedSong.notes.soundCloud || ""
-  })
+    soundcloud: displayedSong.notes.soundCloud || "",
+  });
 
   const handleSubmit = (e: SyntheticEvent) => {
-    e.preventDefault()
-    const newNotes = displayedSong.notes
-    newNotes.lyrics = form.lyrics
-    newNotes.spotifyAlbumCover = form.artwork
-    newNotes.spotifyID = form.spotify.split('/track')[1] == "undefined" ? undefined : form.spotify.split('/track')[1]
-    newNotes.youtubeID = form.youtube.split('/watch?v=')[1] == "undefined" ? undefined : form.youtube.split('/watch?v=')[1]
-    newNotes.soundCloud = form.soundcloud == "undefined" ? undefined : form.soundcloud
+    e.preventDefault();
+    const newNotes = displayedSong.notes;
+    newNotes.lyrics = form.lyrics;
+    newNotes.spotifyAlbumCover = form.artwork;
+    newNotes.spotifyID = !!form.spotify.split("/track")[1]
+      ? form.spotify.split("/track")[1]
+      : undefined;
+    newNotes.youtubeID = !!form.youtube.split("v=")[1]?.split("&")[0]
+      ? form.youtube.split("v=")[1]?.split("&")[0]
+      : undefined;
+    newNotes.soundCloud = !!form.soundcloud ? form.soundcloud : undefined;
     updateSongNotes({
-      title: displayedSong.title,
-      artist: displayedSong.artist,
+      ...displayedSong,
+      set: loggedIn,
       notes: newNotes,
-      set: loggedIn
-    })
+    });
 
-    setOpen(false)
+    setOpen(false);
 
     setDisplayedSong({
       title: displayedSong.title,
       artist: displayedSong.artist,
-      notes: newNotes
-    })
+      notes: newNotes,
+    });
+  };
 
-  }
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
-    e.preventDefault()
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    e.preventDefault();
     changeForm({
       ...form,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
-    <form className="song-edit-form" onSubmit={(e)=> handleSubmit(e)}>
-      <div className='top'>
-        <h2 style = {{fontSize: "40px" }}id="modal-title">Edit Song Info</h2>
-        <h3 style = {{textTransform: "none"}}>Be careful with this! Make sure the previews work before you submit</h3>
-        <input type="submit" id="modal-submit" className="text-button" value="Submit"/>
+    <form className="song-edit-form" onSubmit={(e) => handleSubmit(e)}>
+      <div className="top">
+        <h2 style={{ fontSize: "40px" }} id="modal-title">
+          Edit Song Info
+        </h2>
+        <h3 style={{ textTransform: "none" }}>
+          Be careful with this! Make sure the previews work before you submit
+        </h3>
+        <input
+          type="submit"
+          id="modal-submit"
+          className="text-button"
+          value="Submit"
+        />
       </div>
       <div className="left">
         <h2>lyrics</h2>
         <textarea
-          onChange={(e)=> handleChange(e)}
+          onChange={(e) => handleChange(e)}
           id="lyrics"
           value={form.lyrics}
           name="lyrics"
@@ -65,10 +83,10 @@ const EditSongForm = ({setOpen}) => {
       </div>
       <div className="right">
         <div className="row">
-          <div style={{width: '70%'}}>
+          <div style={{ width: "70%" }}>
             <h2>Youtube</h2>
             <input
-              onChange={(e)=> handleChange(e)}
+              onChange={(e) => handleChange(e)}
               type="text"
               value={form.youtube}
               name="youtube"
@@ -76,19 +94,21 @@ const EditSongForm = ({setOpen}) => {
             />
           </div>
           <iframe
-            className = "youtube-video"
+            className="youtube-video"
             width="150"
             height="100"
-            src={`https://www.youtube.com/embed/${form.youtube.split('watch?v=')[1]}`}
+            src={`https://www.youtube.com/embed/${
+              form.youtube.split("v=")[1]?.split("&")[0]
+            }`}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           />
         </div>
         <div className="row">
-          <div style={{width: '70%'}}>
+          <div style={{ width: "70%" }}>
             <h2>Spotify</h2>
             <input
-              onChange={(e)=> handleChange(e)}
+              onChange={(e) => handleChange(e)}
               type="text"
               value={form.spotify}
               name="spotify"
@@ -96,8 +116,10 @@ const EditSongForm = ({setOpen}) => {
             />
           </div>
           <iframe
-            className = "spotify-embed"
-            src={`https://open.spotify.com/embed/track${form.spotify.split('/track')[1]}`}
+            className="spotify-embed"
+            src={`https://open.spotify.com/embed/track${
+              form.spotify.split("/track")[1]
+            }`}
             width="150"
             height="80"
             frameBorder="0"
@@ -105,23 +127,23 @@ const EditSongForm = ({setOpen}) => {
           />
         </div>
         <div className="row">
-          <div style={{width: '70%'}}>
+          <div style={{ width: "70%" }}>
             <h2>Album Cover</h2>
             <input
-              onChange={(e)=> handleChange(e)}
+              onChange={(e) => handleChange(e)}
               type="text"
               value={form.artwork}
               name="artwork"
               placeholder="Any image url"
             />
           </div>
-          <img src={form.artwork} alt="artwork"/>
+          <img src={form.artwork} alt="artwork" />
         </div>
         <div className="row">
-          <div style={{width: '70%'}}>
+          <div style={{ width: "70%" }}>
             <h2>SoundCloud</h2>
             <input
-              onChange={(e)=> handleChange(e)}
+              onChange={(e) => handleChange(e)}
               type="text"
               value={form.soundcloud}
               name="soundcloud"
@@ -129,7 +151,7 @@ const EditSongForm = ({setOpen}) => {
             />
           </div>
           <iframe
-            className = "soundcloud-embed"
+            className="soundcloud-embed"
             src={form.soundcloud.split('src="')[1]}
             width="25%"
             height="80"
@@ -139,9 +161,9 @@ const EditSongForm = ({setOpen}) => {
         </div>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default EditSongForm
+export default EditSongForm;
 
 // <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/915558670&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/essily" title="مهرجان طب اندال اندال اندال احمد موزه" target="_blank" style="color: #cccccc; text-decoration: none;">مهرجان طب اندال اندال اندال احمد موزه</a> · <a href="https://soundcloud.com/essily/baskotaya" title="مهرجان - انتى بسكوتايه مقرمشة | حسن شاكوش - حمادة مجدى 2020" target="_blank" style="color: #cccccc; text-decoration: none;">مهرجان - انتى بسكوتايه مقرمشة | حسن شاكوش - حمادة مجدى 2020</a></div>
