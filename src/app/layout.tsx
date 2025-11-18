deaa688fdd0b795737e94136216fe6c85acafbcc
Elie Hymowitz <ehymowitz@gmail.com>
2025-11-18 11:34:33 -0500

    feat: global padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-800 text-white`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-800 text-white p-2`}
       >
         {children}
       </body>
